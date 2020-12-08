e9306578f01aa7c3bd4e9764354a7f27ff69e2ab
Chanchalp <piyush.chanchal@hotmail.com>
2020-12-08 06:28:16 +0530

    Almost finished version 1

--- Main.py
+++ Main.py
@@ -32,6 +32,12 @@ class mainWindow():
         self.menu.add_cascade(label="Options",menu=self.addCatMenu)
         self.addCatMenu.add_command(label="Add/Remove Categories",command=self.fnAddCat)
 
+        #####Add Machine Learning options
+        self.addMachineL = Menu(self.menu,tearoff=False)
+        self.menu.add_cascade(label="Machine Learning Options",menu=self.addMachineL)
+        self.addMachineL.add_command(label="Train Model",command=self.modelTraining)
+        self.addMachineL.add_command(label="Learning Settings")
+
         #####Help Menu
         self.helpmenu = Menu(self.menu, tearoff=False)
         self.menu.add_cascade(label='Help', menu=self.helpmenu)
@@ -50,15 +56,20 @@ class mainWindow():
         cnvsStruct.create_rectangle(0,((heightMain*60)/100),((widthMain*80)/100),heightMain,fill="#bfbfbf",outline="")
 
 
+        self.inHrsVal = StringVar()
+        self.inMnsVal = StringVar()
+
+        self.inHrsVal.trace("w", lambda name, index, mode, inHrsVal=self.inHrsVal: self.callback())
+        self.inMnsVal.trace("w", lambda name, index, mode, inHrsVal=self.inMnsVal: self.callback())
 
         #Entry widget for hours and minutes
-        self.inHrs = Entry(root,bg="#f2f2f2",font=("Times",50))
+        self.inHrs = Entry(root,bg="#f2f2f2",font=("Times",50), textvariable=self.inHrsVal)
         self.inHrs.place(x=5,y=30,width=80,height=50)
 
         lblSep = Label(root,text=":",font=("Times",30))
         lblSep.place(x=100,y=40,width=15,height=25)
 
-        self.inMns = Entry(root,bg="#f2f2f2",font=("Times",50))
+        self.inMns = Entry(root,bg="#f2f2f2",font=("Times",50), textvariable=self.inMnsVal)
         self.inMns.place(x=125,y=30,width=100,height=50)
 
         #Label widget for hours and minutes
@@ -114,22 +125,99 @@ class mainWindow():
         self.mainCatList.place(x=400,y=30,width=230)
 
         self.catListUpdate()
-        self.modelTraining()
 
+
+    def callback(self):
+        self.flgModel=0
+        if path.exists("/Users/piyush/Desktop/ProjectsGitHub/ActivitiesLoggerV1/Models/ModelDT1"):
+            self.flgModel=1
+        if self.flgModel==1:
+            self.retrieveAll()
+            print(self.ActivityDoneTimeNumeric)
+            print(self.totalMinutes)
+            predictValues = []
+            for itrModel in range(20):
+                dirname = path.dirname(__file__)
+                savedModel = joblib.load(path.join(str(dirname),"Models/ModelDT"+str(itrModel+1)))
+                predictValues.append(int(savedModel.predict([[self.totalMinutes,self.ActivityDoneTimeNumeric]])))
+            self.CountFrequency(predictValues)
+            # print(self.freqPValues)
+            sortedDictPVal = dict(sorted(self.freqPValues.items(),reverse=True, key=lambda item: item[1]))
+            lstFinalCat = []
+            for itrPredKeys in sortedDictPVal.keys():
+                lstFinalCat.append(itrPredKeys)
+            lstFinalCat = lstFinalCat[0:4]
+            print(lstFinalCat)
+            dfCatWithCode = pd.read_csv("catData/dfCatWithCode.csv")
+            lstFinalCatLbl=[]
+            for top5CatIter in range(4):
+                interimDict = dfCatWithCode[dfCatWithCode.Category_n==lstFinalCat[top5CatIter]]["Category"][0:1].to_dict()
+                for elem in interimDict.values():
+                    lstFinalCatLbl.append(str(elem))
+            print(lstFinalCatLbl)
+
+            firstCat = Label(root,text=lstFinalCatLbl[0],font=("Times",18,"bold"))
+            firstCat.place(x=650,y=75)
+
+            secondCat = Label(root,text=lstFinalCatLbl[1],font=("Times",18,"bold"))
+            secondCat.place(x=650,y=200)
+
+
+            thirdCat = Label(root,text=lstFinalCatLbl[2],font=("Times",18,"bold"))
+            thirdCat.place(x=650,y=350)
+
+
+            fourthCat = Label(root,text=lstFinalCatLbl[3],font=("Times",18,"bold"))
+            fourthCat.place(x=650,y=500)
+
+
+            #print(self.model.predict([[self.totalMinutes,self.ActivityDoneTimeNumeric]]))
+    def CountFrequency(self,my_list): 
+        # Creating an empty dictionary  
+        self.freqPValues = {} 
+        for item in my_list: 
+            if (item in self.freqPValues): 
+                self.freqPValues[item] += 1
+            else: 
+                self.freqPValues[item] = 1
 
     def modelTraining(self):
-        df = pd.read_csv("/Users/piyush/Desktop/ProjectsGitHub/ActivitiesLoggerV1/Data/20201203.csv")
+        listData = os.listdir("/Users/piyush/Desktop/ProjectsGitHub/ActivitiesLoggerV1/Data") # dir is your directory path
+        listDataFileNums = []
+        for dataIter in listData:
+            if str(dataIter).find(".csv")>0:
+                listDataFileNums.append(str(dataIter).replace(".csv",""))
+        listDataFileNums.sort(reverse=True)
+        interimDF = pd.DataFrame()
+        for dataIter in range(len(listDataFileNums)-1):
+            if dataIter==0:
+                interimDF = pd.read_csv("/Users/piyush/Desktop/ProjectsGitHub/ActivitiesLoggerV1/Data/"+str(listDataFileNums[dataIter])+".csv")
+            interimDF = pd.concat([interimDF,pd.read_csv("/Users/piyush/Desktop/ProjectsGitHub/ActivitiesLoggerV1/Data/"+str(listDataFileNums[dataIter+1])+".csv")])
+        
+        dfFinalRows,dfFinalCol = interimDF.shape
+        
+        for itrModel in range(20):
+            df = interimDF.sample(n=int(dfFinalRows/2))
+            self.driver(df,itrModel+1)
+
+
+    def driver(self,df,itrModel):
         df_new = df[['TotalMinutes','ActivityDoneTimeNumeric','Category']]
         inputs = df_new.drop('Category',axis='columns')
         target = df_new[['Category']]
         from sklearn.preprocessing import LabelEncoder
         le_category = LabelEncoder()
         target['Category_n'] = le_category.fit_transform(target['Category'])
+        dfCatWithCode = target
+        dfCatWithCode.to_csv("catData/dfCatWithCode.csv")
         target = target.drop('Category',axis="columns")
         from sklearn import tree
-        model = tree.DecisionTreeClassifier()
-        model.fit(inputs,target)
-        print(model.predict([[400,50.67]]))
+        self.model = tree.DecisionTreeClassifier()
+        self.model.fit(inputs,target)
+        dirname = path.dirname(__file__)
+        filename = path.join(dirname, "Models/ModelDT"+str(itrModel))
+        joblib.dump(self.model,filename)
+
 
 
     def catListUpdate(self):
@@ -151,16 +239,16 @@ class mainWindow():
     
       
     def aboutMenu(self):
-    #    self.AbtWin = Toplevel(root)
+       self.AbtWin = Toplevel(root)
     #    self.AbtWin.overrideredirect(1)
     #    self.AbtWin.configure(bg="#000000",bd=1)
-    #    self.AbtWin.geometry("173x143+300+300")
-    #    self.Msg = Message(self.AbtWin, text="This application is developed in Python by using TextBlob and TKinter library. In case of any concern, please contact to email id Piyush.Chanchal@hotmail.com",bg='#F5F5F5')
-    #    self.Msg.pack()
-    #    self.OKBtn= Button(self.AbtWin,text='OK',width=173,command=self.AbtWin.destroy,bg='#696969',fg='#ffffff')
-    #    self.OKBtn.pack()
-    #    self.AbtWin.mainloop()
-        print("About button has been pressed")
+       self.AbtWin.geometry("200x170")
+       self.Msg = Message(self.AbtWin, text="This application is developed in Python by using Pandas,TKinter and ScikitLearn library. In case of any concern, please contact to email id Piyush.Chanchal@hotmail.com")
+       self.Msg.pack()
+       self.OKBtn= Button(self.AbtWin,text="OK",width=173,command=self.AbtWin.destroy)
+       self.OKBtn.pack()
+       self.AbtWin.mainloop()
+        # print("About button has been pressed")
 
     def fnAddCat(self):
         self.flgRem=0
@@ -243,19 +331,40 @@ class mainWindow():
         self.calWin.destroy()
         self.inTime.destroy()
 
+    def validation(self):
+        flgVal=0
+        if str(self.inHrsVal.get())=="":
+            flgVal=flgVal+1
+        if str(self.inMnsVal.get())=="":
+            flgVal=flgVal+1
+        if str(self.mainCatList.get())=="*Please Select Category":
+            flgVal=flgVal+1
+        if flgVal==0:
+            self.flgError=0
+        elif flgVal>0:
+            messagebox.showerror("ValidationFailed","Hours/Minutes and category fields are mandatory. Please make sure all fields has been responded.")
+            self.flgError=1
 
     def submitButAction(self):
-        print("Submit button has been pressed")
-        self.strYYYYMMDD = str(self.dateTimeNow.strftime("%Y"))+str(self.dateTimeNow.strftime("%m"))+str(self.dateTimeNow.strftime("%d"))
-        print(self.strYYYYMMDD)
+        self.validation()
+        if self.flgError==0:
+            fileList = os.listdir("/Users/piyush/Desktop/ProjectsGitHub/ActivitiesLoggerV1/Data/")
+            numberFiles = len(fileList)
+            if numberFiles>10:
+                messagebox.showinfo("EnoughData","Enough data has been collected. It's time to train a model. Click on 'Machine Learning Options'->'Train Model'")
+            self.strYYYYMMDD = str(self.dateTimeNow.strftime("%Y"))+str(self.dateTimeNow.strftime("%m"))+str(self.dateTimeNow.strftime("%d"))
+            self.retrieveAll()  ##Retrieving all fields data and saving into variables. Then writing it into csv files
+            self.csvWrite()
+            lblSavedSuccess = Label(root,text="Data has been saved on " + str(self.actLoggedOn) + " at " + str(self.actLoggedAt),font=("Times",18))
+            lblSavedSuccess.place(x=50,y=400)
 
-        self.sNum=1
-
-
-        ##Retrieving all fields data and saving into variables. Then writing it into csv files
+    def retrieveAll(self):
         self.hours=str(self.inHrs.get())
         self.minutes=str(self.inMns.get())
-        self.totalMinutes=(int(self.hours)*60)+int(self.minutes)
+        if not(self.hours=="") and not(self.minutes==""):
+            self.totalMinutes=(int(self.hours)*60)+int(self.minutes)
+        else:
+            self.totalMinutes=0
         self.actDoneOn=str(self.lblDateTime.cget("text")[0:self.lblDateTime.cget("text").find("\n")])
         self.actDoneAt=str(self.lblDateTime.cget("text")[self.lblDateTime.cget("text").find("\n")+1:])
         self.actLoggedOn=str(self.dateTimeNow.strftime("%x"))
@@ -268,9 +377,6 @@ class mainWindow():
         self.catValue=self.mainCatList.get()
         self.titleText=self.inTitle.get()
         self.desText=self.inDesc.get("1.0","end-1c")
-        self.csvWrite()
-
-
 
     def csvWrite(self):
         flgCSV = 0
@@ -279,8 +385,8 @@ class mainWindow():
         with open("Data/" + self.strYYYYMMDD + ".csv", 'a', newline='') as csvfile:
             inData = csv.writer(csvfile)
             if flgCSV==0:
-                inData.writerow(['S. No.', 'hours', 'Minutes', 'TotalMinutes','Activity done on(date)', 'Activity done at(time)','ActivityDoneTimeNumeric','Activity logged on(date)', 'Activity logged at(time)','Category','Title','Description'])
-            inData.writerow([self.sNum,self.hours, self.minutes, self.totalMinutes,self.actDoneOn,self.actDoneAt,self.ActivityDoneTimeNumeric,self.actLoggedOn,self.actLoggedAt,self.catValue,self.titleText,self.desText])
+                inData.writerow(['hours', 'Minutes', 'TotalMinutes','Activity done on(date)', 'Activity done at(time)','ActivityDoneTimeNumeric','Activity logged on(date)', 'Activity logged at(time)','Category','Title','Description'])
+            inData.writerow([self.hours, self.minutes, self.totalMinutes,self.actDoneOn,self.actDoneAt,self.ActivityDoneTimeNumeric,self.actLoggedOn,self.actLoggedAt,self.catValue,self.titleText,self.desText])
 
 
 from tkinter import *
@@ -290,8 +396,10 @@ from tkinter import messagebox
 import csv
 import os.path
 from os import path
+from os import listdir
 from tkcalendar import Calendar, DateEntry
 import pandas as pd
+from sklearn.externals import joblib
 
 root = Tk()
 
